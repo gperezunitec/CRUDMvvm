09160200dd657085c1d8fca9a5a2e4603df63a7d
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-07 18:51:12 -0600

    added viewModel

--- CRUDMvvm/Views/ProveedoresView.xaml.cs
+++ CRUDMvvm/Views/ProveedoresView.xaml.cs
@@ -1,9 +1,14 @@
+using CRUDMvvm.ViewModels;
+
 namespace CRUDMvvm.Views;
 
 public partial class ProveedoresView : ContentPage
 {
+	private ProveedoresViewModel viewModel;
 	public ProveedoresView()
 	{
 		InitializeComponent();
+		viewModel = new ProveedoresViewModel();
+		BindingContext=viewModel;
 	}
 }
