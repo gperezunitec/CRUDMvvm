4db0281df79fd4ac3c914d2120ca51e0813c3489
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-08 16:03:11 -0600

    adding views

---
+++ CRUDMvvm/Views/AddEditView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CRUDMvvm.Views;
+
+public partial class AddEditView : ContentPage
+{
+	public AddEditView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CRUDMvvm/Views/MainView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CRUDMvvm.Views;
+
+public partial class MainView : ContentPage
+{
+	public MainView()
+	{
+		InitializeComponent();
+	}
+}
